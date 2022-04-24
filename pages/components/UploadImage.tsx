8b4f40c054b51893553257b396832d6d7acec97b
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-04-24 13:47:32 -0400

    feat: added some styles when dragEnter and dragLeave

---
+++ pages/components/UploadImage.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+interface Props {
+  onDrop: (e: React.DragEvent<HTMLDivElement>) => void;
+}
+
+const UploadImage = ({ onDrop }: Props) => {
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) =>
+    e.preventDefault();
+
+  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.currentTarget.style.opacity = "0.5";
+  };
+
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.currentTarget.style.opacity = "1";
+  };
+
+  return (
+    <div
+      onDragOver={handleDragOver}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDrop={onDrop}
+      className="flex flex-col items-center p-8 outline-2 outline-blue-400/50 outline-dashed rounded-xl w-80 gap-9 bg-neutral-100 select-none transition-opacity"
+    >
+      <div className="pointer-events-none">
+        <Image src="/upload.svg" alt="" width={115} height={90} />
+      </div>
+      <p className="text-gray-4 text-xs">Drag & Drop your image here</p>
+    </div>
+  );
+};
+
+export default UploadImage;
