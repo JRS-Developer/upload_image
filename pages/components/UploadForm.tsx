ab01077415ff40961a07c6a13ea8eaef3d272a66
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-04-24 13:46:08 -0400

    feat: created a UploadImage component

--- pages/components/UploadForm.tsx
+++ pages/components/UploadForm.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import Button from "./Button";
+import UploadImage from "./UploadImage";
 
 interface Props {
   onDrop: (e: React.DragEvent<HTMLDivElement>) => void;
@@ -11,16 +11,7 @@ const UploadForm = ({ onDrop, onChange }: Props) => (
       <h1 className="text-gray-2 text-lg">Upload your image</h1>
       <p className="text-gray-3 text-[10px]">File should be Jpeg, Png...</p>
     </div>
-    <div
-      onDragOver={(e) => e.preventDefault()}
-      onDrop={onDrop}
-      className="flex flex-col items-center p-8 outline-2 outline-blue-400/50 outline-dashed rounded-xl w-80 gap-9 bg-neutral-100 select-none"
-    >
-      <div className="pointer-events-none">
-        <Image src="/upload.svg" alt="" width={115} height={90} />
-      </div>
-      <p className="text-gray-4 text-xs">Drag & Drop your image here</p>
-    </div>
+    <UploadImage onDrop={onDrop} />
     <span className="text-gray-4 text-xs">Or</span>
     <label htmlFor="upload-image">
       <input
@@ -30,7 +21,7 @@ const UploadForm = ({ onDrop, onChange }: Props) => (
         className="sr-only"
         onChange={onChange}
       />
-      <Button component="span" variant="success">
+      <Button component="span" variant="primary">
         Choose a file
       </Button>
     </label>
