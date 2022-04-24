1ae367ab617350b4761182cbca211d356ed3703c
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-04-24 13:45:23 -0400

    feat: finished styling the upladed component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Button from "./components/Button";
 import UploadForm from "./components/UploadForm";
 import ProgressBar from "./components/ProgressBar";
+import { MdCheckCircle } from "react-icons/md";
+import toast, { Toaster } from "react-hot-toast";
 
 interface UploadImgResponse {
   url: string;
@@ -51,13 +53,16 @@ const Home: NextPage = () => {
     file && uploadImage(file);
   };
 
-  const handleCopyClick = () => image && navigator.clipboard.writeText(image);
+  const handleCopyClick = () => {
+    image && navigator.clipboard.writeText(image);
+    toast.success("Copied to clipboard");
+  };
 
   if (loading)
     return (
       <Container>
-        <div className="flex flex-col gap-7">
-          <p className="text-lg">Uploading...</p>
+        <div className="flex flex-col gap-7 w-80">
+          <p className="text-lg text-gray-2">Uploading...</p>
           <ProgressBar />
         </div>
       </Container>
@@ -66,11 +71,32 @@ const Home: NextPage = () => {
   if (image) {
     return (
       <Container>
-        <h3>Uploaded Successfully!</h3>
-        <Image src={image} height={500} width={500} alt="Uploaded image" />
-        <div>
-          <p>{image}</p>
-          <Button onClick={handleCopyClick}>Copy Link</Button>
+        <Toaster />
+        <div className="flex flex-col w-96 gap-4 items-center">
+          <MdCheckCircle className="text-success" size={35} />
+          <h3 className="text-lg text-gray-2">Uploaded Successfully!</h3>
+          <div className="overflow-hidden rounded-xl relative w-full h-80 my-4">
+            <Image
+              src={image}
+              layout="fill"
+              alt="Uploaded image"
+              objectFit="cover"
+              priority
+              placeholder="blur"
+              blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
+            />
+          </div>
+          <div className="flex w-full items-center gap-4 bg-neutral-100 p-0.5 border border-gray-5 rounded-lg">
+            <input
+              readOnly
+              className="overflow-hidden text-ellipsis text-[8px] text-gray-2 flex-1 ml-1.5 resize-none"
+              defaultValue={image}
+              type="text"
+            />
+            <Button onClick={handleCopyClick}>
+              <span className="text-[8px]">Copy Link</span>
+            </Button>
+          </div>
         </div>
       </Container>
     );
